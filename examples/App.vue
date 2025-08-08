e4ed7fbf0697defe2f0e3b6277e4214d525d6107
yuchen <2364184627@qq.com>
2025-08-08 09:06:19 +0800

    feat: 重新添加已清理的App.vue文件，移除了所有敏感token信息

---
+++ examples/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div id="map" class="map"></div>
+  </div>
+</template>
+
+<script>
+// 从环境变量获取天地图 token
+import { Map, View } from 'ol'
+import TileLayer from 'ol/layer/Tile'
+import { XYZ } from 'ol/source'
+import { fromLonLat } from 'ol/proj'
+
+export default {
+  name: 'App',
+  mounted() {
+    // 从环境变量获取 token，如果没有则使用占位符
+    const tiandituToken = process.env.VUE_APP_TIANDITU_TOKEN || 'YOUR_TIANDITU_TOKEN_HERE'
+    
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: `http://t0.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${tiandituToken}`
+          })
+        }),
+        new TileLayer({
+          source: new XYZ({
+            url: `http://t0.tianditu.gov.cn/cva_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${tiandituToken}`
+          })
+        })
+      ],
+      view: new View({
+        center: fromLonLat([116.404, 39.915]),
+        zoom: 10
+      })
+    })
+  }
+}
+</script>
+
+<style>
+.map {
+  width: 100%;
+  height: 500px;
+}
+</style>
